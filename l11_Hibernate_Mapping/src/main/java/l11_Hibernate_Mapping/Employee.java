f1c0ed2d243114dd39fd944b62a370865410a464
aditya-dua <adityaduatechm@gmail.com>
2023-12-09 09:16:45 +0530

    Hibernate Mapping
    Hibernate Mapping

---
+++ l11_Hibernate_Mapping/src/main/java/l11_Hibernate_Mapping/Employee.java
@@ -0,0 +1,46 @@
+package l11_Hibernate_Mapping;
+
+public class Employee {
+	
+	private int id;
+	private String name;
+	private int salary;
+	private Address address;
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getSalary() {
+		return salary;
+	}
+	public void setSalary(int salary) {
+		this.salary = salary;
+	}
+	public Address getAddress() {
+		return address;
+	}
+	public void setAddress(Address address) {
+		this.address = address;
+	}
+	public Employee(int id, String name, int salary) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.salary = salary;
+	}
+	public Employee() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+
+}
---
+++ l11_Hibernate_Mapping/src/main/java/l11_Hibernate_Mapping/One2OneMain.java
@@ -0,0 +1,34 @@
+package l11_Hibernate_Mapping;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+
+public class One2OneMain {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Address add = new Address("Delhi", "Delhi", "Delhi");
+		
+		Employee emp = new Employee(0, "Aditya", 1000);
+		add.setEmp(emp);
+		emp.setAddress(add);
+		
+		
+		
+		Configuration config= new Configuration();
+		
+		SessionFactory sf = config.configure("hbm.cfg.xml").buildSessionFactory();
+		
+		System.out.println("Session Factory::"+sf);
+		
+		Session session = sf.openSession();
+		
+		//session.save(add);
+		session.save(emp);
+		session.close();
+	}
+
+}
