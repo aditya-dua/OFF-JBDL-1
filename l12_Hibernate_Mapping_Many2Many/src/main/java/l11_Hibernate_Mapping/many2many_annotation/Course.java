af3bba9815d481b69f23e660e4c250f1c123e834
aditya-dua <adityaduatechm@gmail.com>
2023-12-10 10:13:45 +0530

    Hibernate Many2many
    Hibernate Many2many

---
+++ l12_Hibernate_Mapping_Many2Many/src/main/java/l11_Hibernate_Mapping/many2many_annotation/Course.java
@@ -0,0 +1,56 @@
+package l11_Hibernate_Mapping.many2many_annotation;
+
+import java.util.Set;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name="m2mA-course")
+public class Course {
+	
+	@Id
+	@GeneratedValue
+	@Column(name = "id")
+	private int id;
+	@Column(name = "name")
+	private String name;
+	@Column(name = "description")
+	private String desc;
+	
+	//private Set<Teacher> teacher;
+	public Course(String name, String desc) {
+		super();
+		this.name = name;
+		this.desc = desc;
+	}
+	public Course() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getDesc() {
+		return desc;
+	}
+	public void setDesc(String desc) {
+		this.desc = desc;
+	}
+	
+	
+	
+
+}
--- l12_Hibernate_Mapping_Many2Many/src/main/java/l11_Hibernate_Mapping/many2many_annotation/Teacher.java
+++ l12_Hibernate_Mapping_Many2Many/src/main/java/l11_Hibernate_Mapping/many2many_annotation/Teacher.java
@@ -1,4 +1,4 @@
-package l11_Hibernate_Mapping.one2many_annotation;
+package l11_Hibernate_Mapping.many2many_annotation;
 
 import java.util.Set;
 
@@ -6,58 +6,63 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name="o2mA-employee")
-public class Employee {
+@Table(name="m2mA-teacher")
+public class Teacher {
+	
 	@Id
 	@GeneratedValue
 	@Column(name = "id")
 	private int id;
-	
 	@Column(name = "name")
 	private String name;
-	
 	@Column(name = "salary")
 	private int salary;
 	
-	@OneToMany(mappedBy = "emp")
-	private Set<Address> address;
+	@ManyToMany(targetEntity = Course.class)
+	@JoinTable(name="m2mA-teacher_course_mapping",
+			joinColumns = {@JoinColumn(name="teacher_id")},
+			inverseJoinColumns = {@JoinColumn(name="course_id")})
+	private Set<Course> courses;
 	
 	public int getId() {
 		return id;
 	}
+
 	public void setId(int id) {
 		this.id = id;
 	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public int getSalary() {
 		return salary;
 	}
+
 	public void setSalary(int salary) {
 		this.salary = salary;
 	}
-	
-	public Set<Address> getAddress() {
-		return address;
+
+	public Set<Course> getCourses() {
+		return courses;
 	}
-	public void setAddress(Set<Address> address) {
-		this.address = address;
+
+	public void setCourses(Set<Course> courses) {
+		this.courses = courses;
 	}
-	public Employee(int id, String name, int salary) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.salary = salary;
-	}
-	public Employee() {
+
+	public Teacher() {
 		super();
 		// TODO Auto-generated constructor stub
 	}
